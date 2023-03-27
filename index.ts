4942a54d3b09fe87071a7ce00203d13cb9edc7a1
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2023-03-26 21:01:21 -0600

    bot running

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
 import { Bot } from 'grammy';
 dotenv.config();
-
-const port = process.env.PORT;
 const token: any = process.env.TELEGRAM_TOKEN;
-
-const app: Express = express();
 const bot = new Bot(token);
 
-app.get("/", (req: Request, res: Response) => res.send("Hello world"));
-app.listen(port, () => console.log(`[Telegram bot ⚡] LISTENING at port: ${port}`));
+bot.command("start", (ctx) => ctx.reply("Welcome up and running"));
+bot.command("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.start();
