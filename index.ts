b16ba9adfc3c0eb379be563f73402926cf71687d
Luis E. Jiménez Robles <luisejimenezrobles@gmail.com>
2023-08-05 19:59:57 -0600

    packages updated

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
 import { Bot } from 'grammy';
 dotenv.config();
+
 const token: any = process.env.TELEGRAM_TOKEN;
+console.log(token)
 const bot = new Bot(token);
 
 bot.command("start", (ctx) => ctx.reply("Welcome up and running"));
